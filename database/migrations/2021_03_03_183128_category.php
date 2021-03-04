ef08226650cbd0cd9c4531ee09118ccfe3e16acb
wins0k <74212779+wins0k@users.noreply.github.com>
2021-03-04 04:11:29 +0300

    add menu && cart && products

---
+++ database/migrations/2021_03_03_183128_category.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Category extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category', function (Blueprint $table) {
+            $table->id();
+            $table->integer('parent')->nullable();
+            $table->string('code');
+            $table->string('title');
+            $table->integer('is_new')->nullable();
+            $table->integer('status')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category');
+    }
+}
